35ed6621336f98a3c8d31899a7a1833751ce8d07
HuJyunKai <contact@sivilization.com>
2019-03-20 15:09:40 +0800

    Remove some useless code generate by MCreator.

---
+++ main/java/mod/mcreator/mcreator_VarListparallelworld.java
@@ -0,0 +1,10 @@
+package mod.mcreator;
+
+public class mcreator_VarListtestenvironmentmod {
+
+	public static int AWorldDimID = 212;
+	public static int ANetherDimID = 213;
+	public static int AWorldSeedOffset = 0;
+	public static int ANetherSeedOffset = 0;
+
+}
